fbf3a56e032b53676bf1e1673ffb4df2330cc5a4
Bartek <bartek@Barteks-MacBook-Pro.local>
2017-02-13 17:49:05 -0800

    updated port name

--- server.js
+++ server.js
@@ -40,4 +40,4 @@ app.get('/api/pushToDB', (req, res)=> {
 })
 // start server
 app.listen(3000);
-console.log('Listening on port ' + PORT + '...');
+console.log('Listening on port ' + port + '...');
