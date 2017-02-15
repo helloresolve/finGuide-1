7c5f8a321c254e5f11c6a54511349a6059c30f77
Loaner - Admin <loaner-admin@Loaners-MacBook-Pro.local>
2017-02-15 13:30:44 -0800

    added back option to allow to save

--- server.js
+++ server.js
@@ -45,12 +45,14 @@ app.use('/', express.static(path.join(__dirname, '/public')));
 app.post('/api/pushToDB', (req, res)=> {
   
   let sessionId = {sessionId: req.body.session};
-  //let options = { upsert: true };
+  let options = { upsert: true };
+
+
 
 
   // email provided
   if (req.body.moduleID === '394253' ) {
-    User.findOneAndUpdate(sessionId, {email: req.body.reply}, function(err, result) {
+    User.findOneAndUpdate(sessionId, {email: req.body.reply}, options, function(err, result) {
       console.log(result)
     })
   };
@@ -58,56 +60,56 @@ app.post('/api/pushToDB', (req, res)=> {
 
   // know where to start
   if (req.body.moduleID === '394313' ) {
-    User.findOneAndUpdate(sessionId, {knowWhereToStart: req.body.reply}, function(err, result) {
+    User.findOneAndUpdate(sessionId, {knowWhereToStart: req.body.reply}, options, function(err, result) {
      console.log(result)
     })
   };
 
   // total debt amount
   if (req.body.moduleID === '394231' ) {
-    User.findOneAndUpdate(sessionId, {totalDebt: req.body.reply}, function(err, result) {
+    User.findOneAndUpdate(sessionId, {totalDebt: req.body.reply}, options, function(err, result) {
       console.log(result);
     })
   };
 
   // average interest
   if (req.body.moduleID === '394235' ) {
-    User.findOneAndUpdate(sessionId, {averageInterestRate: req.body.reply}, function(err, result) {
+    User.findOneAndUpdate(sessionId, {averageInterestRate: req.body.reply}, options, function(err, result) {
       console.log(result);
     })
   };
 
   // monthly debt payments
   if (req.body.moduleID === '394236' ) {
-    User.findOneAndUpdate(sessionId, {monthlyDebtPayments: req.body.reply}, function(err, result) {
+    User.findOneAndUpdate(sessionId, {monthlyDebtPayments: req.body.reply}, options, function(err, result) {
       console.log(result);
     })
   };
 
   // income (y/n)
   if (req.body.moduleID === '394074' ) {
-      User.findOneAndUpdate(sessionId, {incomeYN: req.body.reply}, function(err, result) {
+      User.findOneAndUpdate(sessionId, {incomeYN: req.body.reply}, options, function(err, result) {
       console.log(result);
     })
   };
 
   // income amount
   if (req.body.moduleID === '394076' ) {
-    User.findOneAndUpdate(sessionId, {incomeAmount: req.body.reply}, function(err, result) {
+    User.findOneAndUpdate(sessionId, {incomeAmount: req.body.reply}, options, function(err, result) {
       console.log(result);
     })
   };
 
   // income consistency
   if (req.body.moduleID === '394088' ) {
-    User.findOneAndUpdate(sessionId, {incomeConsistency: req.body.reply}, function(err, result) {
+    User.findOneAndUpdate(sessionId, {incomeConsistency: req.body.reply}, options, function(err, result) {
       console.log(result);
     })
   };
 
   // situation detail
   if (req.body.moduleID === '396899' ) {
-    User.findOneAndUpdate(sessionId, {situationDetail: req.body.reply}, function(err, result) {
+    User.findOneAndUpdate(sessionId, {situationDetail: req.body.reply}, options, function(err, result) {
       console.log(result);
     })
   };
