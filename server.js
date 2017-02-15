fc6a61563a3752d7422c6c4552659f46e01d2eba
Loaner - Admin <loaner-admin@Loaners-MacBook-Pro.local>
2017-02-15 12:56:00 -0800

    trying again

--- server.js
+++ server.js
@@ -52,10 +52,12 @@ app.post('/api/pushToDB', (req, res)=> {
 
   // email provided
   if (req.body.moduleID === '394253' ) {
-    User.findOneAndUpdate(sessionId, {email: req.body.reply}, options, function(err, result) {
-      if(!result){
-        User.save({sessionId: req.body.session}, {email: req.body.reply}, function(err, result2) {
-          console.log(result2);
+    //if session already exists, update it with new info
+    User.findOneAndUpdate(sessionId, {email: req.body.reply}, options, function(err, resultOfFoundRecord) {
+      //if session DOES NOT exist, save session and info
+      if(!resultOfFoundRecord){
+        User.save({sessionId: req.body.session}, {email: req.body.reply}, function(err, resultOfNewRecord) {
+          console.log(resultOfNewRecord);
         })
       }
     })
@@ -64,10 +66,12 @@ app.post('/api/pushToDB', (req, res)=> {
 
   // know where to start
   if (req.body.moduleID === '394313' ) {
-    User.findOneAndUpdate(sessionId, {knowWhereToStart: req.body.reply}, options, function(err, result) {
-      if(!result){
-        User.save({sessionId: req.body.session}, {knowWhereToStart: req.body.reply}, function(err, result2) {
-          console.log(result2);
+    //if session already exists, update it with new info
+    User.findOneAndUpdate(sessionId, {knowWhereToStart: req.body.reply}, options, function(err, resultOfFoundRecord) {
+      //if session DOES NOT exist, save session and info
+      if(!resultOfFoundRecord){
+        User.save({sessionId: req.body.session}, {knowWhereToStart: req.body.reply}, function(err, resultOfNewRecord) {
+          console.log(resultOfNewRecord);
         })
       }
     })
