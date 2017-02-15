c1ffc3fbe86d3cc58443adf789e505b5183dc114
Bartek <bartek@Barteks-MacBook-Pro.local>
2017-02-14 17:47:03 -0800

    removed null

--- server.js
+++ server.js
@@ -45,15 +45,15 @@ app.post('/api/pushToDB', (req, res)=> {
     if (!user) {
         console.log("not found, creating new session...")
         User.create({ sessionId: req.body.session,                          
-                      email: null,
-                      knowWhereToStart: null,
-                      totalDebt: null,
-                      averageInterestRate: null,
-                      monthlyDebtPayments: null,
-                      incomeYN: null,
-                      incomeAmount: null,
-                      incomeConsistency: null,
-                      situationDetail: null
+                      email: "",
+                      knowWhereToStart: "",
+                      totalDebt: "",
+                      averageInterestRate: "",
+                      monthlyDebtPayments: "",
+                      incomeYN: "",
+                      incomeAmount: "",
+                      incomeConsistency: "",
+                      situationDetail: ""
                     }, function (err, response) {
 
           // email included since needs to only add first response after session created
