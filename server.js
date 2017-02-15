ad7ef96756a5e3648afc6051c74d9b09b09040d5
Loaner - Admin <loaner-admin@Loaners-MacBook-Pro.local>
2017-02-15 13:36:23 -0800

    defautl value for string

--- server.js
+++ server.js
@@ -15,16 +15,16 @@ mongoose.connect('mongodb://finGuide:Finguide123@ds157667.mlab.com:57667/heroku_
 
 // define user schema
 var userSchema = mongoose.Schema({
-  sessionId: String,
-  email: String,
-  knowWhereToStart: String,
-  totalDebt: String,
-  averageInterestRate: String,
-  monthlyDebtPayments: String,
-  incomeYN: String,
-  incomeAmount: String,
-  incomeConsistency: String,
-  situationDetail: String,
+  sessionId: {type:String, default: null},
+  email: {type:String, default: null},
+  knowWhereToStart: {type:String, default: null},
+  totalDebt: {type:String, default: null},
+  averageInterestRate: {type:String, default: null},
+  monthlyDebtPayments: {type:String, default: null},
+  incomeYN: {type:String, default: null},
+  incomeAmount: {type:String, default: null},
+  incomeConsistency: {type:String, default: null},
+  situationDetail: {type:String, default: null}
 });
 
 
